b8819e985ff7bb1f4517c437450531c5642d9eae
aliseeyou <pablo.apa3002@gmail.com>
2018-01-10 23:15:43 +0100

    actualizacion

--- test_if.py
+++ test_if.py
@@ -1,8 +1,24 @@
 
-eat = ("salchichas")
-user_input = input("que desea comer: ")
+libro_fant = ("Geralt de Rivia")
+libro_inf = ("Geronimo Stilton")
+canal_inf = ("Nate Gentile")
+canal_rndm = ("Pew Die Pie")
+libro_dist = ("1984")
+user_dlvry = input("Que desea saber: ")
 
-if eat == user_input:
-    print("aqui tiene sus salchichas")
-else:
-    print("no quedan (vaya programa xdd)")
+if user_dlvry == "libro de fantasia":
+    print(libro_fant)
+
+if user_dlvry == "libro infantil":
+    print(libro_inf)
+if user_dlvry == "canal de informatica":
+    print(canal_inf)
+
+if user_dlvry == "canal random":
+    print(canal_rndm)
+
+if user_dlvry == "libro ditopico":
+    print(libro_dist)
+
+if user_dlvry == "BEST YOUTUBER EVER":
+    print("NATE GENTILE")
